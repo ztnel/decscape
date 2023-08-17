dbb35211acc04fc346d423ecf15d200ee8d15d61
ztnel <christian911@sympatico.ca>
2023-08-17 01:54:22 -0400

    stash

--- README.md
+++ README.md
@@ -6,3 +6,9 @@ Requirements:
  - mtgtop8 scraping
  - plot card distributions
  - mainboard and sideboard metadata
+
+Planned:
+ - card date timestamps
+ - card popularity among decks (basics excluded) normalized by maximum copies per deck (4)
+ - deck page iteration (mtgtop8)
+ - plotting tool to visualize card representation over time
--- decscape/__main__.py
+++ decscape/__main__.py
@@ -4,13 +4,25 @@ from decscape.constants import *
 from decscape.scraper import pull_all, pull_dec_file
 from decscape.compiler import update, export
 
-
 """
+Deck Table
 {
-    'Island': {
-        'main': 45
-        'side': 12
-    }
+    cards: [
+        id: 33423432
+    ]
+    date: 04/5/23,
+}
+"""
+"""
+Card Table
+{
+    'id': 3217839127,
+    'name': 'Fatal Push',
+    'counts': 45,
+    'decks': 32,
+    'main': 14,
+    'side': 12,
+    'trend': counts / decks * 4
 }
 """
 logging.basicConfig()
